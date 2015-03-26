d00aa1f6bfb4a2bec5c4af2f571e9cf9449d52ce
Ironedde <edvin.akerfeldt@hotmail.com>
2015-03-26 02:03:58 +0100

    Lets count the balls

--- Standard Assets/Testing Stuff/Scripts/CreateBalls.cs
+++ Standard Assets/Testing Stuff/Scripts/CreateBalls.cs
@@ -5,17 +5,27 @@ using System;
 [Serializable]
 public class CreateBalls : MonoBehaviour {
 
+	/*Remove the comments for the code to remove 
+	 * clutter in the inspector if you choose to  
+	 * show the list of balls in the inspector (not default)
+	 * And remove code above the comment
+	 */
 	public GameObject toCreate;
 	public Vector3 position;
-	public int layers;
-	public int width;
+	public int layers = 5;
+	public int width = 5;
 	public float distance = 2.0f;
 	[SerializeField]
 	private int TotalBalls;
 	private int balsCreated;
 
+	[HideInInspector]
 	[SerializeField]
 	private List<SerializableList> balls = new List<SerializableList>();
+
+	[HideInInspector]
+	[SerializeField]
+	private List<GameObject> ballParents = new List<GameObject>();
 	
 	// Use this for initialization
 	void Start () {
@@ -27,10 +37,16 @@ public class CreateBalls : MonoBehaviour {
 	
 	}
 	public void Create(){
+		TotalBalls = CountBalls();
+		if(toCreate == null){
+			throw new UnassignedReferenceException("Enter a Object to create");
+		}
 		if((layers*(width*width))>4000){
 			print("TO MANY BALS!!");
 		}
 		else{
+			GameObject ballParent = new GameObject("Balls");
+			ballParents.Add(ballParent);
 			SerializableList toAdd = new SerializableList();
 			Vector3 currentPos = position;
 			for(int i=0;i<layers;i++){
@@ -38,6 +54,7 @@ public class CreateBalls : MonoBehaviour {
 					currentPos.z = position.z+o;
 					for(int p = 0;p<width;p++){
 						GameObject tmp = (GameObject)Instantiate(toCreate,currentPos,Quaternion.identity);
+						tmp.transform.parent = ballParent.transform;
 						toAdd.Add(tmp);
 						balsCreated++;
 						currentPos.z += distance;
@@ -52,18 +69,60 @@ public class CreateBalls : MonoBehaviour {
 			TotalBalls += balsCreated;
 			balsCreated = 0;
 			balls.Add(toAdd);
+			//balls.Add(ballParent);
 		}
 	}
 	public void DeletePrevious(){
+		TotalBalls = CountBalls();
 		if(balls.Count == 0){
+			if(ballParents.Count >0){
+				ballParents.Clear();
+			}
 			return;
 		}
-		int last = balls.Count-1;
-		for(int i = 0;i<balls[last].Count();i++){
-			DestroyImmediate(balls[last].Get(i));
+		int balls_last = balls.Count-1;
+		int parent_last = ballParents.Count-1;
+
+		//Destroys the ballgroups parent (Destroys all the balls in the group) 
+		DestroyImmediate(ballParents[parent_last]);
+
+		TotalBalls -= layers*(width*width);
+
+		//Removes the now empty ball list and the parent
+		balls.RemoveAt(balls_last);
+		ballParents.RemoveAt(parent_last);
+
+		#region
+		/*Use this for a less clutered inspector 
+		(if you choose to show the list, not default)*/
+		/*
+		if(balls.Count() == 0){
+			return;
+		}
+		int last = balls.Count()-1;
+		for(int i = 0;i<balls.Count();i++){
+			DestroyImmediate(balls.Get(i));
 		}
 		TotalBalls -= layers*(width*width);
 		balls.RemoveAt(last);
+		*/
+		#endregion
+	}
+	private int CountBalls(){
+		int tmpCounter = 0;
+		GameObject[] array;
+		foreach(SerializableList ballGroup in balls){
+			array = ballGroup.Copy();
+			foreach(GameObject obj in array){
+				if(obj != null){
+					tmpCounter += 1;
+				}
+				else{
+					ballGroup.Remove(obj);
+				}
+			}
+		}
+		return tmpCounter;
 	}
 }  
 
@@ -78,7 +137,10 @@ public class SerializableListGeneric<type>{
 	public void Add(type input){
 		list.Add(input);
 	}
-	public void Remove(int i){
+	public void Remove(type obj){
+		list.Remove(obj);
+	}
+	public void RemoveAt(int i){
 		list.RemoveAt(i);
 	}
 	/*
@@ -99,5 +161,13 @@ public class SerializableListGeneric<type>{
 	}
 	public Boolean isEmpty(){
 		return list.Count>0;
+	}
+	public List<type> GetAll(){
+		return list;
+	}
+	public type[] Copy(){
+		type[] array = new type[list.Count];
+		list.CopyTo(array);
+		return array;
 	}
 }
